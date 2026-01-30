ac60b2103a33e917c98007e613df290da310b64f
Jyoti Kashyap <kashyapjyoti082000@gmail.com>
2026-01-30 12:57:46 +0530

    Day 3: Data types

---
+++ 3-DataType.py
@@ -0,0 +1,10 @@
+# Learning Data Type in Python
+# Data Types
+
+# 1.Text Data type1
+# String 
+Product_name = "Laptop"
+print("Product_name:",Product_name)
+print("Product Datatype is:",type(Product_name))
+
+2.
