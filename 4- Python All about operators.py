78ebfc74ef5003fdac826a060e3863af0a1d900a
Jyoti Kashyap <kashyapjyoti082000@gmail.com>
2026-01-31 16:37:25 +0530

    Day 4: Operators

---
+++ 4- Python All about operators.py
@@ -0,0 +1,41 @@
+# # 1.Arithmetic Operators
+# num1 = 40
+# num2 = 8
+
+# print(num1 - num2)     #Subtarction 
+# print(num1 * num2 )    #Multiplication
+
+# 2.Assigenment Operators
+# value = 50 
+# print(value)
+
+
+# value -= 10 
+# print(value)
+
+# # # 3.Comparison operator
+# p1 = 12
+# p2 = 18
+
+# print(p1>p2)
+# print(p1<p2)
+
+# # 4.Logical Operator
+x1=5
+x2=15
+x3=25
+x4=35
+
+print(x1<x2 and x3<x4)    #And condition
+print("Or Result:", x1>x2 or x3 > x4)  #OR condition
+
+
+# # 5.Identity Operator
+m1 = 100
+m2 = 100
+print(m1 is m2)
+
+
+# # 6.Membership Operator
+
+print("Check 'e' in 'mango':",'o' in 'mango')
